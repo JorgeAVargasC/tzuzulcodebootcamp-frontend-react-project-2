a04dc0ed99acc9c1b82102522ecd98b1d92f7d8e
JorgeVargas <javargas216@unicauca.edu.co>
2022-05-31 23:37:28 -0500

    Favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Navlinks from "./Navlinks";
+import Logo from "../assets/svg/Logo.svg";
 
 export default function Navbar() {
 	return (
-		<div className="p-2 flex">
-			<h1>GS</h1>
+		<div className="flex bg-neutral-800 text-white">
+			<img src={Logo} alt="grocery store logo" />
 			<Navlinks />
 		</div>
 	);
