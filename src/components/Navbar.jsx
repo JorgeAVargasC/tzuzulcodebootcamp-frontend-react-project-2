55b20242961b7ebd23b261b0f7fbfac0d94ac599
LauraMamian <lauramamianc@unicauca.edu.co>
2022-06-02 21:22:46 -0500

    home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,63 @@
-import React from "react";
-import Navlinks from "./Navlinks";
+import React, { useState } from "react";
+import { useContext } from "react";
+import { authContext } from "../context/AuthContext";
+import { Spin as Hamburger } from "hamburger-react";
 import Logo from "../assets/svg/Logo.svg";
 
 export default function Navbar() {
+
+	const [isOpen, setOpen] = useState(false);
+
 	return (
-		<div className="flex flex-row bg-black">
-			<div className="bg-red-700">
-				<img className="" src={Logo} alt="Guitar Store logo" />
+		<div className="content-center">
+			<div className="max-w-6xl mx-auto px-4">
+				<div className="flex justify-between">
+					<div className="flex space-x-7 my-5">
+						<div>
+							<img classNameName="" src={Logo} alt="Guitar Store logo" />
+						</div>
+
+						<div className="hidden md:flex items-center space-x-1">
+							<a href="" className="py-4 px-2 text-orange-500 border-b-4 border-orange-500 font-semibold ">Home</a>
+							<a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-orange-500 transition duration-300">About</a>
+							<a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-orange-500 transition duration-300">Shop</a>
+							<a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-orange-500 transition duration-300">Contact Us</a>
+						</div>
+					</div>
+
+					<div className="hidden md:flex items-center space-x-3 ">
+						<a href="" className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-orange-500 hover:text-white transition duration-300">Log In</a>
+						<a href="" className="py-2 px-2 font-medium text-white bg-orange-500 rounded hover:bg-orange-400 transition duration-300">Sign Up</a>
+					</div>
+
+					<div class="md:hidden flex items-center">
+						<nav
+							className={isOpen ? "navlinks-mobile open" : "navlinks-mobile close"}
+							onClick={() => setOpen(false)}
+						></nav>
+
+						<Hamburger
+							rounded
+							toggled={isOpen}
+							toggle={setOpen}
+							direction="left"
+							duration={0.4}
+						/>
+
+					</div>
+				</div>
 			</div>
-			<div className="flex justify-end bg-yellow-300 mx-auto text-white">
-				<Navlinks />
-			</div>
+
+
 		</div>
+
+		// <div classNameName="flex flex-row bg-black">
+		// 	<div classNameName="bg-red-700">
+		// 		<img classNameName="" src={Logo} alt="Guitar Store logo" />
+		// 	</div>
+		// 	<div classNameName="flex justify-end bg-yellow-300 mx-auto text-white">
+		// 		<Navlinks />
+		// 	</div>
+		// </div>
 	);
 }
