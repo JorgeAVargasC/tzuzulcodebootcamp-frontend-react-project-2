0c7043e68b97bc37fa2a1c2b57eec53d5ce01ada
JorgeVargas <javargas216@unicauca.edu.co>
2022-06-05 19:59:47 -0500

    Counter Reducer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useReducer } from "react";
+import counterReducer, { initialState } from "../reducers/counterReducer";
 
 export default function Home() {
-  return (
-    <div className='p-3'>Home</div>
-  )
+	const [state, dispatch] = useReducer(counterReducer, initialState);
+	return (
+		<>
+			<p>{state.counter}</p>
+
+			<button
+				className="bg-green-300"
+				onClick={() => {
+					dispatch({ type: "ADD" });
+				}}
+			>
+				Add
+			</button>
+			<button
+				className="bg-red-300"
+				onClick={() => {
+					dispatch({ type: "SUB" });
+				}}
+			>
+				Sub
+			</button>
+		</>
+	);
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,14 +2,14 @@ import React, { useState, useContext } from "react";
 import { post } from "../api";
 import Errors from "../components/Errors";
 import { FcGoogle } from "react-icons/fc";
+import { FaFacebookSquare } from "react-icons/fa";
 import { authContext } from "../context/Auth";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+	const { setUser } = useContext(authContext);
+	const navigate = useNavigate();
 
-	const {setUser} = useContext(authContext);
-    const navigate = useNavigate()
-	
 	const [errors, setErrors] = useState({
 		isErrors: false,
 		errors: [],
@@ -17,36 +17,35 @@ export default function SignUp() {
 
 	const signup = (event) => {
 		event.preventDefault();
-		const {name, email, password } = event.target;
+		const { name, email, password } = event.target;
 
 		post("/api/auth/signup", {
-            name:name.value,
+			name: name.value,
 			email: email.value,
 			password: password.value,
 		})
-			.then(({user}) => {
+			.then(({ user }) => {
 				setUser({
 					logged: true,
-					user
+					user,
 				});
-                navigate("/")
+				navigate("/");
 			})
 			.catch((error) => {
 				setErrors({
 					isErrors: true,
-					errors: error.errors.map(e=>e.message),
+					errors: error.errors.map((e) => e.message),
 				});
 			});
 	};
 
 	return (
 		<>
-			
 			<form
 				className="my-10 bg-zinc-800 border-zinc-600 p-5 w-1/2 mx-auto flex flex-col gap-5 shadow-md border rounded text-black"
 				onSubmit={signup}
 			>
-                <input className="p-2 shadow-md" type="text" name="name" />
+				<input className="p-2 shadow-md" type="text" name="name" />
 				<input className="p-2 shadow-md" type="email" name="email" />
 				<input className="p-2 shadow-md" type="password" name="password" />
 				<button className="bg-orange-600 text-white p-2">Sign Up</button>
@@ -58,7 +57,13 @@ export default function SignUp() {
 			>
 				<FcGoogle /> Google Sign Up
 			</a>
-			
+			<a
+				className="flex items-center gap-3 bg-zinc-800 w-max mx-auto p-3 my-10 shadow-md"
+				href="https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/facebook"
+			>
+				<FaFacebookSquare /> Facebook Sign Up
+			</a>
+
 			<Errors errors={errors} />
 		</>
 	);
