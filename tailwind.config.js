4921010a08424dbac191cf7f5a45cc8633727953
LauraMamian <lauramamianc@unicauca.edu.co>
2022-06-05 22:11:10 -0500

    change bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-primary': '#282D35',
+      }
+    },
   },
   plugins: [],
 }
