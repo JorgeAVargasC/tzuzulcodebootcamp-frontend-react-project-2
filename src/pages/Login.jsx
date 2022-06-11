017f0e17818787799989ad32173299ecefb4966b
LauraMamian <lauramamianc@unicauca.edu.co>
2022-06-11 17:19:36 -0500

    floating label

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,64 +7,72 @@ import { authContext } from "../context/Auth";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-	const {setUser} = useContext(authContext)
+    const { setUser } = useContext(authContext)
 
     const navigate = useNavigate()
 
-    const [errors,setErrors] = useState({
-        isErrors:false,
-        errors:[]
+    const [errors, setErrors] = useState({
+        isErrors: false,
+        errors: []
     })
 
     // State form
-    const [data,setData] = useState({
-        email:"",
-        password:""
+    const [data, setData] = useState({
+        email: "",
+        password: ""
     })
 
-    const handleFormChange = (event)=>{
+    const handleFormChange = (event) => {
         // console.log(event.target)
-        const {name,value} = event.target
+        const { name, value } = event.target
         setData({
             ...data,
-            [name]:value
+            [name]: value
         })
     }
 
-    const login = (event)=>{
+    const login = (event) => {
         event.preventDefault()
         // const {email,password} = event.target
 
-        post("/api/auth/login",data).then(({user})=>{
-            setUser({type:'LOGIN',payload:user}) //dispatch
+        post("/api/auth/login", data).then(({ user }) => {
+            setUser({ type: 'LOGIN', payload: user }) //dispatch
             navigate("/")
         })
-        .catch(error=>{
-            setErrors({
-                isErrors:true,
-                errors:error.errors
+            .catch(error => {
+                setErrors({
+                    isErrors: true,
+                    errors: error.errors
+                })
             })
-        })
 
-	}
+    }
 
-	return (
-		<>
-			<form onSubmit={login} className="bg-zinc-800 border-zinc-600 p-5 w-1/2 mx-auto my-10 flex flex-col gap-5 shadow-md border rounded text-black">
-				<input onChange={handleFormChange} className="p-2 shadow-md" type="email" name="email" placeholder="jhon-doe@gs.com"/>
-				<input onChange={handleFormChange} className="p-2 shadow-md" type="password" name="password" placeholder="password"/>
-				<button className="bg-orange-600 text-white p-2">Iniciar sesión</button>
-			</form>
+    return (
+        <>
+            <form onSubmit={login} className="bg-zinc-800 border-zinc-600 p-5 w-1/2 mx-auto my-10 flex flex-col gap-5 shadow-md border rounded text-black">
+                {/* <input onChange={handleFormChange} className="p-2 shadow-md" type="email" name="email" placeholder="jhon-doe@gs.com" /> */}
+                {/* <input onChange={handleFormChange} className="p-2 shadow-md" type="password" name="password" placeholder="password" /> */}
+                <div class="relative">
+                <input onChange={handleFormChange} type="email" name="email" id="floating_outlined" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border-1 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-orange-600 focus:outline-none focus:ring-0 focus:border-orange-600 peer" placeholder=" " />
+                    <label for="floating_outlined" class="absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-transparent px-2 peer-focus:px-2 peer-focus:text-orange-600 peer-focus:dark:text-orange-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">E-mail</label>
+                </div>
+                <div class="relative">
+                    <input onChange={handleFormChange} type="password" name="password" id="floating_outlined" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border-1 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-orange-600 focus:outline-none focus:ring-0 focus:border-orange-600 peer" placeholder=" " />
+                    <label for="floating_outlined" class="absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-transparent px-2 peer-focus:px-2 peer-focus:text-orange-600 peer-focus:dark:text-orange-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Password</label>
+                </div>
+                <button className="bg-orange-600 text-white p-2 rounded-lg">Iniciar sesión</button>
+            </form>
 
-			<a href="https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/google" className="flex items-center gap-3 bg-zinc-800 rounded text-white w-max mx-auto p-3 my-10 shadow-md">
-				<FcGoogle /> Google Login
-			</a>
+            <a href="https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/google" className="flex items-center gap-3 bg-zinc-800 rounded text-white w-max mx-auto p-3 my-10 shadow-md">
+                <FcGoogle /> Google Login
+            </a>
 
-			<a href="https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/facebook" className="flex items-center gap-3 bg-zinc-800 w-max mx-auto p-3 my-10 shadow-md">
-				<FaFacebookSquare /> Facebook Login
-			</a>
-			
-			<Errors errors={errors} />
-		</>
-	);
+            <a href="https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/facebook" className="flex items-center gap-3 bg-zinc-800 w-max mx-auto p-3 my-10 shadow-md">
+                <FaFacebookSquare /> Facebook Login
+            </a>
+
+            <Errors errors={errors} />
+        </>
+    );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -46,10 +46,22 @@ export default function SignUp() {
 	return (
 		<>
 			<form onSubmit={signup} className="my-10 bg-zinc-800 border-zinc-600 p-5 w-1/2 mx-auto flex flex-col gap-5 shadow-md border rounded text-black">
-				<input type={type} value={value} onChange={onChange} className="p-2 shadow-md" name="name" placeholder="Jhon Doe"/>
-				<input className="p-2 shadow-md" {...email} name="email" placeholder="jhon-doe@gs.com"/>
-				<input className="p-2 shadow-md" {...password} name="password" placeholder="Password"/>
-				<button className="bg-orange-600 text-white p-2">Sign Up</button>
+				{/* <input type={type} value={value} onChange={onChange} className="p-2 shadow-md" name="name" placeholder="Jhon Doe"/> */}
+                <div class="relative">
+                <input type={type} value={value} onChange={onChange} id="floating_outlined" name="name" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border-1 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-orange-600 focus:outline-none focus:ring-0 focus:border-orange-600 peer" placeholder=" " />
+                    <label for="floating_outlined" class="absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-transparent px-2 peer-focus:px-2 peer-focus:text-orange-600 peer-focus:dark:text-orange-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Name</label>
+                </div>
+				{/* <input className="p-2 shadow-md" {...email} name="email" placeholder="jhon-doe@gs.com"/> */}
+                <div class="relative">
+                <input {...email} id="floating_outlined" name="email" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border-1 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-orange-600 focus:outline-none focus:ring-0 focus:border-orange-600 peer" placeholder=" " />
+                    <label for="floating_outlined" class="absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-transparent px-2 peer-focus:px-2 peer-focus:text-orange-600 peer-focus:dark:text-orange-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">E-mail</label>
+                </div>
+				{/* <input className="p-2 shadow-md" {...password} name="password" placeholder="Password"/> */}
+                <div class="relative">
+                    <input {...password} id="floating_outlined" name="password" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border-1 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-orange-600 focus:outline-none focus:ring-0 focus:border-orange-600 peer" placeholder=" " />
+                    <label for="floating_outlined" class="absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-transparent px-2 peer-focus:px-2 peer-focus:text-orange-600 peer-focus:dark:text-orange-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Password</label>
+                </div>
+				<button className="bg-orange-600 text-white p-2 rounded-lg">Sign Up</button>
 			</form>
 
 			<a href="https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/google" className="flex items-center gap-3 bg-zinc-800 rounded text-white w-max mx-auto p-3 my-10 shadow-md">
